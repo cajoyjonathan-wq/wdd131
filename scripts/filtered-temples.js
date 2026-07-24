d317b131ef03278a803457408681c75dd533e855
cajoyjonathan-wq <jcajoy@byupathway.edu>
2026-07-24 18:29:39 +0800

    applied filters and designed css

---
+++ scripts/filtered-temples.js
@@ -0,0 +1,186 @@
+const year = document.querySelector("#currentyear");
+
+const today = new Date();
+
+year.innerHTML = `${today.getFullYear()}`;
+
+document.getElementById("lastModified").innerHTML = `Last Modification:  ${document.lastModified}.`
+
+const hamburger = document.querySelector('.hamburger-menu');
+const navLink = document.querySelector('.nav-link');
+
+hamburger.addEventListener('click', () => {
+    navLink.classList.toggle('open')
+
+    if (navLink.classList.contains('open')) {
+        hamburger.textContent = 'X'
+    } else {
+        hamburger.textContent = '☰'
+    }
+});
+
+
+const temples = [
+    {
+        templeName: "Aba Nigeria",
+        location: "Aba, Nigeria",
+        dedicated: "2005, August, 7",
+        area: 11500,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+    },
+    {
+        templeName: "Manti Utah",
+        location: "Manti, Utah, United States",
+        dedicated: "1888, May, 21",
+        area: 74792,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+    },
+    {
+        templeName: "Payson Utah",
+        location: "Payson, Utah, United States",
+        dedicated: "2015, June, 7",
+        area: 96630,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+    },
+    {
+        templeName: "Yigo Guam",
+        location: "Yigo, Guam",
+        dedicated: "2020, May, 2",
+        area: 6861,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+    },
+    {
+        templeName: "Washington D.C.",
+        location: "Kensington, Maryland, United States",
+        dedicated: "1974, November, 19",
+        area: 156558,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+    },
+    {
+        templeName: "Lima Perú",
+        location: "Lima, Perú",
+        dedicated: "1986, January, 10",
+        area: 9600,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+    },
+    {
+        templeName: "Mexico City Mexico",
+        location: "Mexico City, Mexico",
+        dedicated: "1983, December, 2",
+        area: 116642,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+    },
+    {
+        templeName: "Manila Philippines",
+        location: "Quezon City, Manila",
+        dedicated: "1984, September, 25-27",
+        area: 26683,
+        imageUrl:
+            "https://churchofjesuschristtemples.org/assets/img/temples/manila-philippines-temple/manila-philippines-temple-68248.jpg"
+    },
+    {
+        templeName: "Davao Philippines",
+        location: "Davao City, Davao del Sur",
+        dedicated: "2026, May, 3",
+        area: 18450,
+        imageUrl:
+            "https://churchofjesuschristtemples.org/assets/img/temples/davao-philippines-temple/davao-philippines-temple-69513-main.jpg"
+    },
+    {
+        templeName: "Cebu City Philippines",
+        location: "Cebu City, Cebu",
+        dedicated: "2010, June, 13",
+        area: 29556,
+        imageUrl:
+            "https://churchofjesuschristtemples.org/assets/img/temples/_temp/133-Cebu-City-Philippines-Temple.jpg"
+    }
+];
+
+const templePictures = document.querySelector('.temple-pictures')
+
+
+function createTempleCard(filteredTemples) {
+    document.querySelector(".temple-pictures").innerHTML = "";
+    filteredTemples.forEach((temple) => {
+        const card = document.createElement("section");
+        const templeName = document.createElement("h2");
+        const location = document.createElement("p");
+        const dedicated = document.createElement("p");
+        const area = document.createElement("p");
+        const imageUrl = document.createElement("img");
+
+        templeName.textContent = temple.templeName;
+        location.textContent = `Location: ${temple.location}`;
+        dedicated.textContent = `Dedicated: ${temple.dedicated}`;
+        area.textContent = `Area: ${temple.area} sq ft`;
+
+        imageUrl.src = temple.imageUrl;
+        imageUrl.alt = temple.templeName;
+        imageUrl.loading = "lazy";
+
+        card.appendChild(templeName);
+        card.appendChild(location);
+        card.appendChild(dedicated);
+        card.appendChild(area);
+        card.appendChild(imageUrl);
+
+        templePictures.appendChild(card);
+    }
+    )
+};
+
+createTempleCard(temples);
+
+const oldLink = document.querySelector("#old");
+
+oldLink.addEventListener("click", () => {
+    const oldTemples = temples.filter(temple => {
+        const year = temple.dedicated.split(",")[0];
+        return year < 1900;
+    })
+    createTempleCard(oldTemples);
+});
+
+const newLink = document.querySelector("#new");
+
+newLink.addEventListener("click", () => {
+    const newTemples = temples.filter(temple => {
+        const year = temple.dedicated.split(",")[0];
+        return year > 1900;
+    })
+    createTempleCard(newTemples);
+});
+
+const largeLink = document.querySelector("#large");
+
+largeLink.addEventListener("click", () => {
+    const largeTemples = temples.filter(temple => {
+        const area = temple.area;
+        return area > 90000;
+    })
+    createTempleCard(largeTemples);
+});
+
+const smallLink = document.querySelector("#small");
+
+smallLink.addEventListener("click", () => {
+    const smallTemples = temples.filter(temple => {
+        const area = temple.area;
+        return area < 10000;
+    })
+    createTempleCard(smallTemples);
+});
+
+const homeLink = document.querySelector("#home");
+
+homeLink.addEventListener("click", () => {
+    createTempleCard(temples);
+});
+
